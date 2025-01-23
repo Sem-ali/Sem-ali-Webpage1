b2c6108fe71f7a691ffc686dfbfc73f7d733f5d8
Sem-ali <118457767+Sem-ali@users.noreply.github.com>
2025-01-23 19:02:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('hello');
+console.log('i like burger');
+
+window.alert('this is an alert!');
+window.alert('i like banana!');
+
+document.getElementById("myH1").textContent= 'Hello';
+document.getElementById("myP").textContent= 'i like burger';
+
+
+// this is a comment
+
+/*
+this
+is
+a
+comment
+*/
